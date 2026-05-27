e59098f243a44fb4163be25bd6717d965f9f0e45
Eunice <davilaeunice3@gmail.com>
2026-05-26 23:58:58 -0600

    Corrección en las variables de la base de datos

--- config/db.js
+++ config/db.js
@@ -2,10 +2,15 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const conexion = mysql.createPool({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    user: process.env.DB_USER || process.env.MYSQLUSER,
+    
+    password: process.env.DB_PASS || process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    
+    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+    
+    port: process.env.MYSQLPORT || 3306, 
     multipleStatements: true
 });
 
